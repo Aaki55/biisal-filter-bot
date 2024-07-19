63f6e6a262f16c934c5164672e2943346f87680e
Aaki55 <aaki849056@gmail.com>
2024-07-19 20:31:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ API_HASH = environ.get('API_HASH', '3f779654fdd3b9278fd95869aabf4593')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7252430326').split()]
-USERNAME = environ.get('USERNAME', "https://telegram.me/Aaki551")
+USERNAME = environ.get('USERNAME', "https://telegram.me/DeoxysX")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002211393413'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+4RB2-U2o9yE4ZmQ9')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002221865155').split()]
